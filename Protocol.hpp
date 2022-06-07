00b9c9e4489085451c38147f7765cc598965d261
NickSot <nikola.s.sotirov@gmail.com>
2022-06-07 14:41:24 +0300

    remove redundant variables

--- Protocol.hpp
+++ Protocol.hpp
@@ -16,11 +16,6 @@ class Protocol {
 	public:
 		Protocol () {};
 		virtual void send_file(const char * filename) = 0;
-		
-	protected:
-		unsigned int port;
-		std::string ip_address;
-		SOCKET sock;
 };
 
 #endif
